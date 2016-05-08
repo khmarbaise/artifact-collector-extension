dc1716ab73c52a7d05910be06760d1a01b8cf0e9
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-05-08 13:22:35 +0200

    First Commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Maven Artifact Collector
+
+[![Apache License, Version 2.0, January 2004](https://img.shields.io/github/license/khmarbaise/maven-artifact-collector.svg?label=License)](http://www.apache.org/licenses/)
+[![Maven Central](https://img.shields.io/maven-central/v/com.soebes.extensions/maven-artifact-collector.svg?label=Maven%20Central)](http://search.maven.org/#search%7Cga%7C1%7Cmaven-buildtime-profiler)
+[![Build Status](https://travis-ci.org/mojohaus/sql-maven-plugin.svg?branch=master)](https://travis-ci.org/khmarbaise/maven-artifact-collector)
+
+
+[1]: http://maven.apache.org/ref/3.0.3/maven-core/apidocs/org/apache/maven/eventspy/AbstractEventSpy.html
---
+++ src/main/java/com/soebes/maven/extensions/ArtifactCollector.java
@@ -0,0 +1,149 @@
+package com.soebes.maven.extensions;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.maven.eventspy.AbstractEventSpy;
+import org.apache.maven.execution.MavenExecutionResult;
+import org.eclipse.aether.RepositoryEvent;
+import org.eclipse.aether.RepositoryEvent.EventType;
+import org.eclipse.aether.artifact.Artifact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Singleton;
+
+/**
+ * @author Karl Heinz Marbaise <khmarbaise@apache.org>
+ */
+@Named
+@Singleton
+public class ArtifactCollector
+    extends AbstractEventSpy
+{
+    private final Logger LOGGER = LoggerFactory.getLogger( getClass() );
+
+    private List<String> collectedArtifacts = Collections.synchronizedList( new LinkedList<String>() );
+
+    @Inject
+    public ArtifactCollector()
+    {
+        LOGGER.debug( "ArtifactCollector ctor called." );
+    }
+
+    @Override
+    public void init( Context context )
+        throws Exception
+    {
+        super.init( context );
+
+        LOGGER.info( "Maven Artifact Collector Version {} started.", ArtifactCollectorVersion.getVersion() );
+
+    }
+
+    @Override
+    public void onEvent( Object event )
+        throws Exception
+    {
+        try
+        {
+            if ( event instanceof org.eclipse.aether.RepositoryEvent )
+            {
+                repositoryEventHandler( (RepositoryEvent) event );
+            }
+            else if ( event instanceof MavenExecutionResult )
+            {
+                executionResultEventHandler( (MavenExecutionResult) event );
+            }
+            else
+            {
+                // TODO: What kind of event we haven't considered?
+                LOGGER.debug( "Event {}", event.getClass().getCanonicalName() );
+            }
+        }
+        catch ( Exception e )
+        {
+            LOGGER.error( "Exception", e );
+        }
+    }
+
+    @Override
+    public void close()
+    {
+        LOGGER.debug( "done." );
+    }
+
+    private String getGAVFromArtifact( Artifact artifact)
+    {
+        StringBuilder sb = new StringBuilder( artifact.getGroupId() );
+        sb.append( ":" );
+        sb.append( artifact.getArtifactId() );
+        sb.append( ":" );
+        sb.append( artifact.getVersion() );
+        sb.append( ":" );
+        sb.append( artifact.getExtension() );
+        if ( !artifact.getClassifier().isEmpty() )
+        {
+            sb.append( ":" );
+            sb.append( artifact.getClassifier() );
+        }
+        return sb.toString();
+    }
+
+    private void repositoryEventHandler( org.eclipse.aether.RepositoryEvent repositoryEvent )
+    {
+        EventType type = repositoryEvent.getType();
+        switch ( type )
+        {
+            case ARTIFACT_INSTALLED:
+                collectedArtifacts.add( getGAVFromArtifact( repositoryEvent.getArtifact() ) );
+                break;
+
+            case ARTIFACT_DESCRIPTOR_INVALID:
+            case ARTIFACT_DESCRIPTOR_MISSING:
+            case METADATA_INVALID:
+            case ARTIFACT_RESOLVING:
+            case ARTIFACT_RESOLVED:
+            case METADATA_RESOLVING:
+            case METADATA_RESOLVED:
+            case ARTIFACT_DOWNLOADING:
+            case ARTIFACT_DOWNLOADED:
+            case METADATA_DOWNLOADING:
+            case METADATA_DOWNLOADED:
+            case ARTIFACT_INSTALLING:
+            case METADATA_INSTALLING:
+            case METADATA_INSTALLED:
+            case ARTIFACT_DEPLOYING:
+            case ARTIFACT_DEPLOYED:
+            case METADATA_DEPLOYING:
+            case METADATA_DEPLOYED:
+                // Those events are not recorded.
+                break;
+
+            default:
+                LOGGER.error( "repositoryEventHandler {}", type );
+                break;
+        }
+    }
+
+    private void executionResultEventHandler( MavenExecutionResult event )
+    {
+        if (collectedArtifacts.isEmpty()) {
+            return;
+        }
+            
+        // TODO: Use better formatting
+        LOGGER.info( "--             Maven Artifact Collector Summary                       --" );
+        LOGGER.info( "------------------------------------------------------------------------" );
+        for ( String item : collectedArtifacts )
+        {
+            LOGGER.info( item );
+        }
+
+    }
+
+}
